1f594ca609146a7e9b28ca63bf1b15707a15203f
tusharkapse7779-dotcom <tusharkapse7779@gmail.com>
2026-04-30 18:24:32 +0530

    Delete js directory

--- js/evm.js
+++
@@ -1,3 +0,0 @@
-export function castVote(id, party) {
-    alert("Vote cast for " + party);
-}
--- js/voice.js
+++
@@ -1,4 +0,0 @@
-export function triggerVoiceReport() {
-    const speech = new SpeechSynthesisUtterance("Election update running");
-    speechSynthesis.speak(speech);
-}
