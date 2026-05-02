caa8ce482a2b5c46b48bbc285458e63576da0429
tusharkapse7779-dotcom <tusharkapse7779@gmail.com>
2026-05-02 12:37:43 +0530

    Refactor service worker for cleaner syntax

--- sw.js
+++ sw.js
@@ -1,18 +1,9 @@
-// Simple service worker for PWA offline capability
 const CACHE_NAME = 'election-pro-v1';
-const urlsToCache = [
-  './',
-  './index.html'
-];
+const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
